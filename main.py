db695fb7d7870a6ce556997bdaa7f7dff16e6f59
Rudhra0811 <rudhranethra.alampally@gmail.com>
2024-07-22 15:26:25 +0530

    amazon

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import tldextract
 from bs4 import BeautifulSoup
 import cv2
 import numpy as np
+from prettytable import PrettyTable
 
 def get_soup(url, headers):
     session = requests.Session()
@@ -18,6 +19,24 @@ def show_captcha(captcha_url):
     cv2.destroyAllWindows()
     captcha_input = input("Please enter captcha: ")
     return captcha_input
+
+def amazon(soup, url, table):
+    # Getting the name of the product
+    product_name = soup.find("span", {"id": "productTitle"})
+
+    # Getting the price of the product
+    rupee_symbol = soup.find("span", {"class": "a-price-symbol"})
+    product_price = soup.find("span", {"class": "a-price-whole"})
+
+    if product_name and rupee_symbol and product_price:
+        # Adding the details to the table
+        table.add_row([tldextract.extract(url).domain.strip(), product_name.text.strip(), rupee_symbol.text + product_price.text])
+    else:
+        print(f"Failed to extract product details from {url}")
+
+# Initialize the table
+table = PrettyTable()
+table.field_names = ["Domain", "Product Name", "Price"]
 
 # Take Input
 url1 = input("Enter URL: ")
@@ -43,7 +62,10 @@ captcha2 = soup2.find("form", {"name": "captchaForm"})
 
 if captcha1 is None:
     print("Lucky! No Captcha on site 1")
-    print("Title of the page 1: ", soup1.title.string)
+    if 'amazon' in url1:
+        amazon(soup1, url1, table)
+    else:
+        print(f"Site 1 ({url1}) is not Amazon")
 else:
     print("Captcha found on site 1")
     captcha_image_url1 = soup1.find("img", {"id": "captchaTag"}).get("src")
@@ -52,9 +74,15 @@ else:
 
 if captcha2 is None:
     print("Lucky! No Captcha on site 2")
-    print("Title of the page 2: ", soup2.title.string)
+    if 'amazon' in url2:
+        amazon(soup2, url2, table)
+    else:
+        print(f"Site 2 ({url2}) is not Amazon")
 else:
     print("Captcha found on site 2")
     captcha_image_url2 = soup2.find("img", {"id": "captchaTag"}).get("src")
     captcha_input2 = show_captcha(captcha_image_url2)
     # Here you would handle sending the captcha response back to the server
+
+# Print the table
+print(table)
